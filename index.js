cc833db39d33ab6fd2f2cd34b1318af348cdeedd
cyberbot143 <krishnasai952@gmail.com>
2020-03-15 01:40:31 -0400

    add gql schema & cors as mdw

--- index.js
+++ index.js
@@ -4,16 +4,37 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const logger = require('koa-logger');
 const bodyParser = require("koa-bodyparser")
+const cors = require('@koa/cors');
+const { ApolloServer, gql } = require("apollo-server-koa");
 const app = new Koa();
 const router = new Router();
 const userRouter = new Router({ prefix: "/users" })
-const cors = require('@koa/cors');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => "Hello world!"
+    }
+};
+
+const formatResponse = (response, args) => {
+    console.log("queryString : ", args.queryString);
+    console.log("variables : ", args.variables);
+    return response;
+};
+
+const gqlServer = new ApolloServer({ typeDefs, resolvers, formatResponse });
+
 
 app.use(logger());
-
 app.use(cors())
-
 app.use(bodyParser());
+
 //error handling purpose
 app.use(async (ctx, next) => {
     try {
@@ -27,19 +48,28 @@ app.use(async (ctx, next) => {
 
 
 
+
+
+
+
 require('./routes/index')({ router });
-
-
-
 app.use(router.routes()).use(router.allowedMethods());
 
-require('./routes/user')({ userRouter })
 
+
+require('./routes/user')({ userRouter })
 app.use(userRouter.routes()).use(userRouter.allowedMethods({ throw: true }))
+
+
+
+
 
 app.on('error', (err, ctx) => {
     console.error('server error', err, ctx)
 });
+
+gqlServer.applyMiddleware({ app, path: "/graphql" });
+
 
 const server = app.listen(PORT, () => {
     console.log(`Running on ${PORT}`)
