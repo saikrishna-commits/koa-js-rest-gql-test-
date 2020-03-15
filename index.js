514e30eacb9d931a8c838df7181aa4e7e849ff10
cyberbot143 <krishnasai952@gmail.com>
2020-03-15 11:36:17 -0400

    add koa json to pretiffy resp and better comments

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const logger = require('koa-logger');
 const bodyParser = require("koa-bodyparser")
+const prettyJson = require('koa-json')
 const cors = require('@koa/cors');
 const { ApolloServer, gql } = require("apollo-server-koa");
 const app = new Koa();
 const router = new Router();
 const userRouter = new Router({ prefix: "/users" })
+
+
+//* gql types
 
 const typeDefs = gql`
   type Query {
@@ -19,6 +23,7 @@ const typeDefs = gql`
   }
 `;
 
+//* gql resolvers
 const resolvers = {
     Query: {
         hello: () => "Hello world!"
@@ -34,12 +39,7 @@ const formatResponse = (response, args) => {
 const gqlServer = new ApolloServer({ typeDefs, resolvers, formatResponse });
 
 
-if (process.env.NODE_ENV === 'development')
-    app.use(logger());
-app.use(cors())
-app.use(bodyParser());
-
-//error handling purpose
+//* error handling purpose
 app.use(async (ctx, next) => {
     try {
         await next();
@@ -51,7 +51,22 @@ app.use(async (ctx, next) => {
 });
 
 
-//response time mdw using fn generator
+if (process.env.NODE_ENV === 'development') {
+    app.use(logger());
+
+}
+app.use(prettyJson()) //? prettify json response
+app.use(cors()) //* allow requests from anyother origins running on different ports
+
+
+//* add parser mdw (internally listens on req.on("data") and constructs accordingly )
+app.use(bodyParser.json()); //* for parsing application/json
+app.use(bodyParser.urlencoded({ extended: ture })) //* for parsing application/x-www-form-urlencoded
+
+
+
+
+//* response time mdw using fn generator (going to deprecate this pattern)
 
 app.use(function* responseTime(next) {
     const start = new Date;
@@ -59,6 +74,18 @@ app.use(function* responseTime(next) {
     const ms = new Date - start;
     this.set("X-Response-Time", `${ms} ms`);
 })
+
+async function calculateResponseTime(ctx, next) {
+    console.log('Started tracking response time')
+    const started = Date.now()
+    await next()
+    // once all middleware below completes, this continues
+    const ellapsed = (Date.now() - started) + 'ms'
+    console.log('Response time is:', ellapsed)
+    ctx.set('X-ResponseTime', ellapsed)
+}
+
+app.use(calculateResponseTime)
 
 require('./routes/index')({ router });
 app.use(router.routes()).use(router.allowedMethods());
